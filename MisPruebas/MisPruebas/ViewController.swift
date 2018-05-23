f508bb5fdd4ed118c46d55b488d577ebe5816f9c
Pedro Hernandez <pedroh.r99@gmail.com>
2018-05-23 18:23:57 +0200

    Añadir switch para encender y apagar la pantalla

--- MisPruebas/MisPruebas/ViewController.swift
+++ MisPruebas/MisPruebas/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func interruptorPulsado(_ sender: UISwitch) {
+        view.backgroundColor = sender.isOn == true ? .yellow : .darkGray
+    }
 
 }
 
